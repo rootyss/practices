e33a5e04fa53b0ca4e7b604599e628b687f39fdf
rootyss <jaxent@yandex.ru>
2022-02-04 18:34:41 +0300

    add redux devTools

--- redux/src/init.jsx
+++ redux/src/init.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducers from './store/store.js';
@@ -8,7 +8,10 @@ import App from './App.jsx';
 import "./style.scss";
 
 export default () => {
-  const store = createStore(reducers, applyMiddleware(thunk));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunk),
+  ));
 
   return (
     <Provider store={store}>
